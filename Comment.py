9e21d845d651f42510b8e7cece58f81d98335211
vigenjan <vamirkha@uci.edu>
2016-03-11 14:08:16 -0800

    Added files via upload

--- Comment.py
+++ Comment.py
@@ -1,25 +1,28 @@
 # This class defines a comment and keeps track of data for easy usage
 #
-# body:      Str of comment body
-# subreddit: Str of subreddit name, used to verify
-# score:     Int of comment score, how many upvotes it received
+# original_body:      Str of comment body
+# processed_body:     Str of processed body
+# subreddit:          Str of subreddit name, used to verify
+# score:              Int of comment score, how many upvotes it received
+# length:             Int of the number of words
+
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.corpus import stopwords
+from sklearn.feature_extraction import DictVectorizer
+
 class Comment():
     import nltk.data, nltk.tag
 
     # Store parts of speech tagger as a class variable for speed.
     tagger = nltk.tag.PerceptronTagger()
 
-    from nltk.stem.wordnet import WordNetLemmatizer
-    lemmatizer = WordNetLemmatizer()
-
     def __init__(self, object):
         import re
         self.original_body = object['body']
         self.processed_body = re.sub("\s+", " ", re.sub("[^a-z0-9]", " ", object['body'].lower())).strip()
-        self.lemmatized_body = " ".join([Comment.lemmatizer.lemmatize(word) for word in self.processed_body.split()])
         self.subreddit = object['subreddit']
         self.score = object['score']
-        self.length = len(self.processed_body)
+        self.length = len(self.processed_body.split())
 
     def parts_of_speech(self, max_words_to_pos=None):
         # Split the processed_body string into a list elements at every space.
@@ -35,10 +38,30 @@ class Comment():
 
             return " ".join(pos_list)
 
+    def bag_of_words(self):
+        return CountVectorizer(self.processed_body, stop_words=stopwords.words('english'))
+
+
     def features(self):
         return {
-            "body": self.processed_body,
-            "parts_of_speech": self.parts_of_speech(),
-            "score": self.score,
-            "length": self.length
+            "spaghetti_spaghetti": "spaghetti spaghetti" in self.processed_body, #AdviceAnimals
+            "yum_yum": "yum yum" in self.processed_body, #AdviceAnimals
+            "united_states":"United States" in self.processed_body,  #News
+            "r_worldnews": "r wordlnews" in self.processed_body, #News
+            "r_videos": "r videos" in self.processed_body, #videos
+            "watch_v":"watch v" in self.processed_body, #videos
+            "r_funny":"r funny" in self.processed_body, #funny
+            "funny_comments":"funny comments" in self.processed_body, #funny
+            "super_bowl":"super_bowl" in self.processed_body, #nfl
+            "this_season": "this season" in self.processed_body, #nfl
+            "r_leagueoflegends" : "r leagueoflegends" in self.processed_body, #leagueoflegends
+            "gem_gem" :"gem gem" in self.processed_body, #funny
+            "high_school":"high school" in self.processed_body, #todayIlearned
+            "star_wars":"star wars" in self.processed_body, #todayIlearned
         }
+        
+    def featureSparse(self):
+        return DictVectorizer(self.features())
+        
+    def getProcessedBody(self):
+        return self.processed_body
