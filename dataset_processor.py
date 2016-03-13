85fb8c9275d057706bd9a5195c382ad01ae2f041
Nick Morris <nickmorri@gmail.com>
2016-03-13 14:02:06 -0700

    Refactored for simplicity

--- dataset_processor.py
+++ dataset_processor.py
@@ -1,6 +1,11 @@
 import json
 from Comment import Comment
 from Dataset import DataSet
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
+from sklearn.dummy import DummyClassifier
+
 # In this module, we load the dataset, which is in JSON, and parse it. The
 # resulting data is tallied to create a list of Subreddit objects defined as
 # subreddits. Each comment was posted in one of many Subreddits, so as we go
@@ -32,66 +37,51 @@ def load_comments(filename, max_iteration=None):
                 yield Comment(json.loads(line))
 
 
-def evaluate_classifier(classifier, data):
-    classifier.fit(data['training_sparse_matrix'], data['training_labels'])
-    return classifier.test(data['validation_sparse_matrix'], data['validation_labels'])
-
-
 def optimize_params(classifier, data_set, params):
     from sklearn.grid_search import GridSearchCV
     return GridSearchCV(classifier, params, error_score=0).fit(data_set['training_sparse_matrix'], data_set['training_labels'])
 
 
-def general_test(filename, sample_size, n_cross_validation=5, random_seed=None, filter_fn=None):
-    from Classifiers import MultinomialNaiveBayes, LogisticRegression, SupportVectorMachine, MostFrequent
+def general_test(filename, sample_size, classifiers, n_cross_validation=5, random_seed=None, filter_fn=None, verbose=False):
+
+    sets = DataSet([comment for comment in load_comments(filename, sample_size)], random_seed, filter_fn).generate_n_cross_validation_sets(n_cross_validation)
+
+    if verbose:
+        print("Finished generating cross validation sets")
+
+    results = {}
+
     import numpy as np
+    for classifier in classifiers:
+        result = [classifier.fit(set['training_sparse_matrix'], set['training_labels']).score(set['validation_sparse_matrix'], set['validation_labels']) for set in sets],
+        results[type(classifier).__name__] = {"result": result, "average":  np.mean(result)}
+        if verbose:
+            print("Finished testing {}".format(type(classifier).__name__))
 
-    data_set = DataSet([comment for comment in load_comments(filename, sample_size)], random_seed, filter_fn)
-    print("Finished loading data set")
-
-    sets = data_set.generate_n_cross_validation_sets(n_cross_validation)
-    print("Finished generating cross validation sets")
-
-    naive_bayes_results = [evaluate_classifier(MultinomialNaiveBayes(), set) for set in sets]
-    print("Finished testing MultinomialNaiveBayes")
-
-    logistic_regression_results = [evaluate_classifier(LogisticRegression(), set) for set in sets]
-    print("Finished testing LogisticRegression")
-
-    support_vector_machine_results = [evaluate_classifier(SupportVectorMachine(), set) for set in sets]
-    print("Finished testing SupportVectorMachine")
-
-    most_frequent_results = [evaluate_classifier(MostFrequent(), set) for set in sets]
-    print("Finished testing MostFrequent")
-
-    return {
-        "dataset_size": sets[0]['size'],
-        "naive_bayes_average": np.mean(naive_bayes_results),
-        "logistic_regression_average": np.mean(logistic_regression_results),
-        "support_vector_average": np.mean(support_vector_machine_results),
-        "most_frequent_average": np.mean(most_frequent_results),
-        "naive_bayes_results": naive_bayes_results,
-        "logistic_regression_results": logistic_regression_results,
-        "support_vector_machine_results": support_vector_machine_results,
-        "most_frequent_results": most_frequent_results
-    }
+    return (sets[0]['size'], results)
 
 
 def search_for_ideal_threshold():
     filename = "/Users/nick/RC_2015-01_mc10"
-    sample_size = 200000
+    sample_size = 10
     random_seed = None
     n_cross_validation = 5
+    classifiers = [MultinomialNB(), LogisticRegression(), LinearSVC()]
 
-    results = [general_test(filename, sample_size, n_cross_validation, random_seed, lambda comment: comment.length > length_threshold) for length_threshold in range(0, 100, 10)]
+    best = {}
 
-    max_naive_bayes = max(results, key=lambda x:x['naive_bayes_average'])
-    max_logistic_regression = max(results, key=lambda x:x['logistic_regression_average'])
-    max_support_vector_machine = max(results, key=lambda x:x['support_vector_average'])
+    for classifier in classifiers:
+        best[type(classifier).__name__] = {"average": -1, "cutoff": -1}
 
-    print("Best average for Naive Bayes {} occurred at length threshold {}".format(max_naive_bayes['naive_bayes_average'], max_naive_bayes['length_threshold']))
-    print("Best average for Logistic Regression {} occurred at length threshold {}".format(max_logistic_regression['logistic_regression_average'], max_logistic_regression['length_threshold']))
-    print("Best average for Support Vector Machine {} occurred at length threshold {}".format(max_support_vector_machine['support_vector_average'], max_support_vector_machine['length_threshold']))
+    for cutoff in range(0, 100, 20):
+        results = general_test(filename, sample_size, classifiers, n_cross_validation, random_seed, lambda x: x.length > cutoff)
+
+        for key, value in results[1].iteritems():
+            if value['average'] > best[key]['average']:
+                best[key]['average'] = value['average']
+                best[key]['cutoff'] = cutoff
+
+    return best
 
 
 def human_test(filename, sample_size, random_seed=None, filter_fn=None):
@@ -115,18 +105,20 @@ def human_test(filename, sample_size, random_seed=None, filter_fn=None):
     return {"human_average": correct / float(len(data[0]))}
 
 
-
 if __name__ == "__main__":
     filename = "/Users/nick/RC_2015-01_mc10"
-    sample_size = 500000
+    sample_size = 100
+    classifiers = [MultinomialNB(), LogisticRegression(), LinearSVC(), DummyClassifier(strategy="most_frequent")]
+    filter_fn = lambda x: x.length > 0
 
-    results = general_test(filename, sample_size, filter_fn=lambda x: x.length > 0)
+    results = general_test(filename=filename, sample_size=sample_size, classifiers=classifiers, filter_fn=filter_fn, verbose=True)
 
-    print("data set size: {}".format(results['dataset_size']))
-    print("naive bayes average: {}".format(results['naive_bayes_average']))
-    print("logistic regression average: {}".format(results['logistic_regression_average']))
-    print("support vector average: {}".format(results['support_vector_average']))
-    print("most frequent average: {}".format(results['most_frequent_average']))
+    print("data set size: {}".format(results[0]))
+    for key, value in results[1].iteritems():
+        print("{} average: {}".format(key, value['average']))
+
+    # for key, value in search_for_ideal_threshold().iteritems():
+    #     print("Best average for {}: {} occured at length cutoff {}".format(key, value["average"], value["cutoff"]))
 
 
     # data_set = DataSet([comment for comment in load_comments(filename, 1000)])
